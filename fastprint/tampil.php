36c476219b84631f1d427da2ec4f328142ee43e5
salsabillamirza14agatha <101077172+salsabillamirza14agatha@users.noreply.github.com>
2023-07-20 14:48:13 +0700

    tampil.php

--- fastprint/tampil.php
+++ fastprint/tampil.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html>
-<head> <title>PKTJ TEGAL</title></head>
+<head> <title>Fast Print</title></head>
 <body>
-    <h2>DATA TARUNA PKTJ TEGAL</h2>
+    <h2>DATA Produk</h2>
     <br/>
-    <a href="input2.php">+ TAMBAH TARUNA</a>
+    <a href="input2.php">+ TAMBAH Data</a>
     </br>
     </br>
     <table border="1">
         <tr>
-            <th>NOTAR</th>
-            <th>Nama</th>
-            <th>Prodi</th>
-            <th>Jenis Kelamin</th>
-            <th>Alamat</th>
+            <th>NO</th>
+            <th>ID Produk</th>
+            <th>Harga</th>
+            <th>Kategori</th>
+            <th>Status</th>
             <th>Aksi</th>
         </tr>
         <?php
         include 'koneksi.php';
-        $data =mysqli_query ($koneksi,"select * from taruna2");
+        $data =mysqli_query ($koneksi,"select * from mytabel");
         while ($d = mysqli_fetch_array($data)){
             ?>
             <tr>
-                <td><?php echo $d['notar'] ?></td>
-                <td><?php echo $d['nama'] ?></td>
-                <td><?php echo $d['prodi'] ?></td>
-                <td><?php echo $d['jenis_kelamin'] ?></td>
-                <td><?php echo $d['alamat'] ?></td>
+                <td><?php echo $d['id_produk'] ?></td>
+                <td><?php echo $d['nama_produk'] ?></td>
+                <td><?php echo $d['harga'] ?></td>
+                <td><?php echo $d['kategori'] ?></td>
+                <td><?php echo $d['status'] ?></td>
                 <td>
                     <a href="edit.php?id=<?php echo $d['id']; ?>">EDIT</a>
                     <a href="hapus.php?id=<?php echo $d['id']; ?>">HAPUS</a>
